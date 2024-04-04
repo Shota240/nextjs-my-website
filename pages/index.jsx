a104bcb25b26b4f1dc0c34bac69955c966cd3729
Shota240 <shoup.8519@gmail.com>
2024-04-04 09:47:24 +0900

    ✨ カウンターをuseStateで設置

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,16 +4,16 @@ import classes from "styles/Home.module.css";
 import { Links } from "components/Links";
 import { Main } from "components/Main";
 import { Header } from "components/Header";
-import { useCallback, useEffect } from "react";
-import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const handleClick = useCallback((e) => {
-    e.preventDefault();
-    alert(e.target.href);
-  }, []);
+  const [count, setCount] = useState(1);
+
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+  };
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -31,9 +31,10 @@ export default function Home() {
       </Head>
       <Header />
 
-      <Link href="/about" onClick={handleClick}>
+      <h1>{count}</h1>
+      <button href="/about" onClick={handleClick}>
         ボタン
-      </Link>
+      </button>
 
       <main className={`${classes.main} ${inter.className}`}>
         <Main page="index" />
