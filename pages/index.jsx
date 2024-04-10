9939f4caf76c0df05b12640f290a0313fd344013
Shota240 <shoup.8519@gmail.com>
2024-04-10 18:11:32 +0900

    🎨 custom hooksで関数等をまとめる対応

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,51 +4,16 @@ import classes from "styles/Home.module.css";
 import { Links } from "components/Links";
 import { Main } from "components/Main";
 import { Header } from "components/Header";
-import { useCallback, useEffect, useState } from "react";
+import { useCounter } from "hooks/useCounter";
+import { useInputArray } from "hooks/useInputArray";
+import { useBgLightBlue } from "hooks/useBgLightBlue";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(() => {
-    if (count < 10)  {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, [count]);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("5文字以内にしてください");
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some(item => item === text)) {
-        alert('同じ要素が既に存在します。');
-        return prevArray;
-      }
-      const newArray = [...prevArray, text];
-      return newArray;
-    });
-  }, [text]);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
     <>
@@ -61,6 +26,7 @@ export default function Home() {
       {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+
       <input type="text" value={text} onChange={handleChange} />
       <button onClick={handleAdd}>追加</button>
       <ul>
