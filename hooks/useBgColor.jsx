3199c42eb6cda07022efd361cb8defd070eb26e5
Shota240 <shoup.8519@gmail.com>
2024-04-12 10:37:20 +0900

    ✨ useRouterの学習

---
+++ hooks/useBgColor.jsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export const useBgColor = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    document.body.style.backgroundColor = router.pathname === "/" ? "lightblue": "beige";
+
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
+  }, [router.pathname]);
+};
